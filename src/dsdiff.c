7b43b8d9aa732d930f3f5a4dd42cb5abe364e2c9
Kimmo Taskinen <kimmo.taskinen@me.com>
2017-02-07 12:45:03 +0000

    dsdiff: add write support
    Patch by Kimmo Taskinen with some minor changes.

--- src/dsdiff.c
+++ src/dsdiff.c
@@ -28,6 +28,7 @@ struct dsdiff {
 	uint16_t num_channels;
 	uint64_t data_size;
 	uint8_t *buf;
+	uint32_t bit_pos;
 };
 
 #define ID(a, b, c, d) ((a) << 24 | (b) << 16 | (c) << 8 | (d))
@@ -191,16 +192,222 @@ static int dff_stopread(sox_format_t *ft)
 	return SOX_SUCCESS;
 }
 
+#define DFF_MAX_CHANNELS	1000
+#define DFF_CMPR_NAME		"not compressed"
+
+static int dff_writeheader(sox_format_t *ft)
+{
+	struct dsdiff *dff = ft->priv;
+	unsigned chnl_size = 2 + 4 * dff->num_channels;
+	unsigned cmpr_size = 4 + 1 + sizeof(DFF_CMPR_NAME);
+	unsigned prop_size =
+		4 +			/* SND */
+		12 + 4 +		/* FS */
+		12 + chnl_size +
+		12 + cmpr_size;
+	uint64_t frm8_size =
+		4 +			/* DSD */
+		12 + 4 +		/* FVER */
+		12 + prop_size +
+		12 + dff->data_size;
+
+	if (lsx_writedw(ft, ID('F', 'R', 'M', '8')) ||
+	    lsx_writeqw(ft, dff->data_size ?
+			frm8_size + dff->data_size : SOX_UNKNOWN_LEN) ||
+            lsx_writedw(ft, ID('D', 'S', 'D', ' ')))
+		return SOX_EOF;
+
+	if (lsx_writedw(ft, ID('F', 'V', 'E', 'R')) ||
+	    lsx_writeqw(ft, 4) ||
+            lsx_writedw(ft, 0x01050000))
+		return SOX_EOF;
+
+	if (lsx_writedw(ft, ID('P', 'R', 'O', 'P')) ||
+	    lsx_writeqw(ft, prop_size) ||
+	    lsx_writedw(ft, ID('S', 'N', 'D', ' ')))
+		return SOX_EOF;
+
+	if (lsx_writedw(ft, ID('F', 'S', ' ', ' ')) ||
+	    lsx_writeqw(ft, 4) ||
+            lsx_writedw(ft, dff->sample_rate))
+		return SOX_EOF;
+
+	if (lsx_writedw(ft, ID('C', 'H', 'N', 'L')) ||
+            lsx_writeqw(ft, chnl_size) ||
+            lsx_writew(ft, dff->num_channels))
+		return SOX_EOF;
+
+	if (dff->num_channels == 2) {
+		if (lsx_writedw(ft, ID('S', 'L', 'F', 'T')) ||
+		    lsx_writedw(ft, ID('S', 'R', 'G', 'T')))
+			return SOX_EOF;
+	} else {
+		char ch[8];
+		int i;
+
+		for (i = 0; i < dff->num_channels; i++) {
+			snprintf(ch, sizeof(ch), "C%03d", i);
+			if (lsx_writedw(ft, ID(ch[0], ch[1], ch[2], ch[3])))
+				return SOX_EOF;
+		}
+	}
+
+	if (lsx_writedw(ft, ID('C', 'M', 'P', 'R')) ||
+	    lsx_writeqw(ft, cmpr_size) ||
+	    lsx_writedw(ft, ID('D', 'S', 'D', ' ')) ||
+	    lsx_writeb(ft, sizeof(DFF_CMPR_NAME)) ||
+	    lsx_writes(ft, DFF_CMPR_NAME) ||
+	    lsx_writeb(ft, 0))
+		return SOX_EOF;
+
+	if (lsx_writedw(ft, ID('D', 'S', 'D', ' ')) ||
+	    lsx_writeqw(ft, dff->data_size ? dff->data_size : SOX_UNKNOWN_LEN))
+		return SOX_EOF;
+
+	return SOX_SUCCESS;
+}
+
+static int dff_startwrite(sox_format_t *ft)
+{
+	struct dsdiff *dff = ft->priv;
+
+	if (ft->signal.channels > DFF_MAX_CHANNELS) {
+		lsx_fail_errno(ft, SOX_EOF, "too many channels");
+		return SOX_EOF;
+	}
+
+	dff->sample_rate = ft->signal.rate;
+	dff->num_channels = ft->signal.channels;
+	dff->data_size = 0;
+	dff->buf = lsx_malloc(dff->num_channels);
+	if (!dff->buf)
+		return SOX_ENOMEM;
+
+	return dff_writeheader(ft);
+}
+
+static int dff_write_buf(sox_format_t *ft)
+{
+	struct dsdiff *dff = ft->priv;
+	size_t wlen = dff->num_channels;
+
+	if (lsx_write_b_buf(ft, dff->buf, wlen) < wlen)
+		return SOX_EOF;
+
+	memset(dff->buf, 0, wlen);
+
+	return SOX_SUCCESS;
+}
+
+static void dff_write_bits(struct dsdiff *dff, const sox_sample_t *buf,
+                           unsigned start_bit, unsigned len)
+{
+	unsigned i, j;
+
+	for (i = 0; i < dff->num_channels; i++) {
+		unsigned d = dff->buf[i];
+
+		for (j = 0; j < len; j++) {
+			d |= (buf[i + j * dff->num_channels] > 0) <<
+				(7 - j - start_bit);
+		}
+
+		dff->buf[i] = d;
+	}
+}
+
+static size_t dff_write(sox_format_t *ft, const sox_sample_t *buf, size_t len)
+{
+	struct dsdiff *dff = ft->priv;
+	unsigned nchan = dff->num_channels;
+	size_t wsamp = 0;
+
+	len /= nchan;
+
+	if (dff->bit_pos) {
+		unsigned pre = min(len, 8 - dff->bit_pos);
+
+		dff_write_bits(dff, buf, dff->bit_pos, pre);
+
+		dff->bit_pos += pre;
+		buf += pre * nchan;
+		wsamp += pre;
+		len -= pre;
+
+		if (dff->bit_pos == 8) {
+			dff->bit_pos = 0;
+
+			if (dff_write_buf(ft))
+				return 0;
+
+			dff->data_size += dff->num_channels;
+		}
+	}
+
+	while (len >= 8) {
+		dff_write_bits(dff, buf, 0, 8);
+
+		buf += 8 * nchan;
+		wsamp += 8;
+		len -= 8;
+
+		if (dff_write_buf(ft))
+			return wsamp * nchan;
+
+		dff->data_size += dff->num_channels;
+	}
+
+	if (len) {
+		dff_write_bits(dff, buf, 0, len);
+
+		dff->bit_pos = len;
+		wsamp += len;
+	}
+
+	return wsamp * nchan;
+}
+
+static int dff_stopwrite(sox_format_t *ft)
+{
+	struct dsdiff *dff = ft->priv;
+	unsigned i;
+	int err = SOX_SUCCESS;
+
+	if (dff->bit_pos) {
+		unsigned silence = 0x69 & (0xff >> dff->bit_pos);
+		for (i = 0; i < dff->num_channels; i++)
+			dff->buf[i] |= silence;
+
+		err = dff_write_buf(ft);
+	}
+
+	free(dff->buf);
+
+	if (err)
+		return err;
+
+	if (lsx_seeki(ft, 0, SEEK_SET)) {
+		lsx_fail_errno(ft, SOX_EOF,
+			       "error rewinding output to update header");
+		return SOX_EOF;
+	}
+
+	return dff_writeheader(ft);
+}
+
 LSX_FORMAT_HANDLER(dsdiff)
 {
 	static char const * const names[] = { "dff", NULL };
-	static unsigned const write_encodings[] = { 0 };
+	static unsigned const write_encodings[] = {
+		SOX_ENCODING_DSD, 1, 0,
+		0
+        };
 	static sox_format_handler_t const handler = {
 		SOX_LIB_VERSION_CODE,
 		"Direct Stream Digital Interchange File Format (DSDIFF)",
 		names, SOX_FILE_BIG_END,
 		dff_startread, dff_read, dff_stopread,
-		NULL, NULL, NULL,
+		dff_startwrite, dff_write, dff_stopwrite,
 		NULL, write_encodings, NULL,
 		sizeof(struct dsdiff)
 	};
